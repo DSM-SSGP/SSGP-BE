afb57295c8c7fca5ed30f365032cb26db24dd533
jina <jina6962@gmail.com>
2021-09-14 17:20:21 +0900

    [INIT]

---
+++ ssgp/src/main/java/project/ssgp/SsgpApplication.java
@@ -0,0 +1,13 @@
+package project.ssgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgpApplication.class, args);
+	}
+
+}
---
+++ ssgp/src/test/java/project/ssgp/SsgpApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ssgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
