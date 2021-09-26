a4b3c3d7ec5fee8de471574719f1dae5cec82585
jina <jina6962@gmail.com>
2021-09-26 22:28:15 +0900

    [ADD] brand enum

---
+++ ssgp/src/main/java/project/ssgp/entity/product/BrandEnum.java
@@ -0,0 +1,19 @@
+package project.ssgp.entity.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BrandEnum {
+
+    GS25(1, "GS25"),
+    CU(2, "CU"),
+    MINISTOP(3, "MIMISTOP"),
+    SEVEN(4, "세븐일레븐"),
+    EMART24(5, "emart24");
+
+    private final int num;
+
+    private final String name;
+}
