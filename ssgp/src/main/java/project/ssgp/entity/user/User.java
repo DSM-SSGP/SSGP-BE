5dab428bd2b1045f877c750b9fd3d554836432ea
jina <jina6962@gmail.com>
2021-09-26 22:27:36 +0900

    [ADD] user entity

---
+++ ssgp/src/main/java/project/ssgp/entity/user/User.java
@@ -0,0 +1,25 @@
+package project.ssgp.entity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document("tbl_user")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    private String email;
+
+    private String name;
+
+}
