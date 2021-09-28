c0a80677b3851353ecf71cce9a95db130876cf00
jina <jina6962@gmail.com>
2021-09-28 21:28:09 +0900

    [ADD] - user service request

---
+++ ssgp/src/main/java/project/ssgp/payload/user/SignUpRequest.java
@@ -0,0 +1,18 @@
+package project.ssgp.payload.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+
+    private String id;
+
+    private String password;
+
+}
