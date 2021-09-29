5d83d7a8220ed8fe2359600c14d723c74f5a539d
jina <jina6962@gmail.com>
2021-09-29 21:23:55 +0900

    [ADD] - 회원 정보 수정 request

---
+++ ssgp/src/main/java/project/ssgp/payload/request/UpdateInformationRequest.java
@@ -0,0 +1,14 @@
+package project.ssgp.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateInformationRequest {
+
+    private String password;
+
+}
