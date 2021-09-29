1e9cf0d153e1ca994ec25090fcff5d8093186401
jina <jina6962@gmail.com>
2021-09-29 21:22:19 +0900

    [DELETE] - @Document, @id

--- ssgp/src/main/java/project/ssgp/entity/product/Selling.java
+++ ssgp/src/main/java/project/ssgp/entity/product/Selling.java
@@ -4,20 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
-import project.ssgp.entity.product.Product;
 
-import javax.persistence.Id;
-
-@Document("tbl_selling")
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Selling {
-
-    @Id
-    private Product product;
 
     private String content;
 
