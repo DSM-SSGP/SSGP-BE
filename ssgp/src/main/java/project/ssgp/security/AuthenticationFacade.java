3952e385be340a576ea6306fb177f4fe12e2e67f
jina <jina6962@gmail.com>
2021-09-29 23:08:52 +0900

    [FIX]

--- ssgp/src/main/java/project/ssgp/security/AuthenticationFacade.java
+++ ssgp/src/main/java/project/ssgp/security/AuthenticationFacade.java
@@ -1,15 +1,30 @@
 package project.ssgp.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import project.ssgp.entity.user.User;
+import project.ssgp.repository.UserRepository;
 
+@RequiredArgsConstructor
 @Component
 public class AuthenticationFacade {
 
+    private final UserRepository userRepository;
+
     public User getUser() {
-        UserAuthentication authentication = (UserAuthentication) SecurityContextHolder.getContext().getAuthentication();
-        return authentication.getUser();
+        return userRepository.findById(getUserId()).orElse(null);
+    }
+
+    private String getUserId() {
+        return getAuthentication() != null ? (String) getAuthentication().getPrincipal() : "";
+    }
+
+    private static Authentication getAuthentication() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return securityContext.getAuthentication();
     }
 
 }
