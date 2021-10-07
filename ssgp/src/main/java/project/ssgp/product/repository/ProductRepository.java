d576d93c81cdf3ba38a99ca4811e1b59ab126922
jina <jina6962@gmail.com>
2021-10-07 22:59:03 +0900

    [ADD]

---
+++ ssgp/src/main/java/project/ssgp/product/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package project.ssgp.product.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import project.ssgp.product.entity.Product;
+import project.ssgp.user.entity.User;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, Integer> {
+
+    List<Product> findAllByOrderByName();
+
+    List<Product> findAllByOrderByLikeCountDesc();
+
+    List<Product> findAllByLikeUserIdsContaining(String userId);
+
+    List<Product> findAllByProductEnableTrue();
+
+}
