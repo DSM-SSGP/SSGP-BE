0da3de7f3c865c45d384ed915879b122401bdbc4
jina <jina6962@gmail.com>
2021-10-14 22:12:08 +0900

    [ADD] - 좋아요처리 request

---
+++ ssgp/src/main/java/project/ssgp/product/payload/request/LikeRequest.java
@@ -0,0 +1,18 @@
+package project.ssgp.product.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeRequest {
+
+    private Integer productId;
+
+    private String userId;
+
+}
