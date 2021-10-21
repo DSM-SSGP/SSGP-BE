19cb2b0fe78ed208038a5fd4e5ddb59e3ec69bf0
jina <jina6962@gmail.com>
2021-10-21 23:30:43 +0900

    [FIX] - entity class 명 수정

--- ssgp/src/main/java/project/ssgp/user/repository/UserRepository.java
+++ ssgp/src/main/java/project/ssgp/user/repository/UserRepository.java
@@ -2,9 +2,9 @@ package project.ssgp.user.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import project.ssgp.user.entity.User;
+import project.ssgp.user.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 
 }
