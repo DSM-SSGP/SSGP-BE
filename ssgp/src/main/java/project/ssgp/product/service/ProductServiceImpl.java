06e0e696c63778e344368dd5e352b787379402af
jina <jina6962@gmail.com>
2021-10-21 23:29:38 +0900

    [FIX] - entity class 명 수정

--- ssgp/src/main/java/project/ssgp/product/service/ProductServiceImpl.java
+++ ssgp/src/main/java/project/ssgp/product/service/ProductServiceImpl.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.ssgp.exception.UserNotFoundException;
 import project.ssgp.product.entity.BrandEnum;
-import project.ssgp.product.entity.Product;
+import project.ssgp.product.entity.ProductEntity;
 import project.ssgp.product.entity.Selling;
 import project.ssgp.exception.ProductNotFoundException;
 import project.ssgp.product.payload.request.LikeRequest;
@@ -12,12 +12,11 @@ import project.ssgp.product.payload.response.ApplicationListResponse;
 import project.ssgp.product.payload.response.ProductResponse;
 import project.ssgp.product.payload.response.SellingResponse;
 import project.ssgp.product.repository.ProductRepository;
-import project.ssgp.user.entity.User;
+import project.ssgp.user.entity.UserEntity;
 import project.ssgp.user.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,10 +30,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void LikeProduct(LikeRequest likeRequest) {
 
-        User user = userRepository.findById(likeRequest.getUserId())
+        UserEntity user = userRepository.findById(likeRequest.getUserId())
                 .orElseThrow(UserNotFoundException::new);
 
-        Product product = productRepository.findById(likeRequest.getProductId())
+        ProductEntity product = productRepository.findById(likeRequest.getProductId())
                 .orElseThrow(ProductNotFoundException::new);
 
         boolean isLiked = productRepository.existsByLikeUserIds(product, user);
@@ -49,7 +48,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponse getProduct(Integer productId) {
-        Product product = productRepository.findById(productId)
+        ProductEntity product = productRepository.findById(productId)
                 .orElseThrow(ProductNotFoundException::new);
 
         return ProductResponse.builder()
@@ -87,7 +86,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<ProductResponse> getLowPriceProductList() { //최저가 순으로 정렬
-        List<Product> products = productRepository.findAllByProductEnableTrue();
+        List<ProductEntity> products = productRepository.findAllByProductEnableTrue();
         products.sort(Comparator.comparingInt(o -> o.getSellings().stream().map(Selling::getPrice).min(Integer::compareTo).orElse(0)));
         return getProductList(products);
     }
@@ -95,11 +94,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ApplicationListResponse searchProduct(String searchWord) {
 
-        List<Product> productList = productRepository.findAllByName(searchWord);
+        List<ProductEntity> productList = productRepository.findAllByName(searchWord);
 
         List<ProductResponse> productResponses = new ArrayList<>();
 
-        for (Product product : productList) {
+        for (ProductEntity product : productList) {
             Integer price = product.getSellings().stream()
                     .map(Selling::getPrice)
                     .min(Integer::compareTo)
@@ -123,11 +122,11 @@ public class ProductServiceImpl implements ProductService {
                 .build();
     }
 
-    private List<ProductResponse> getProductList(List<Product> productList) {
+    private List<ProductResponse> getProductList(List<ProductEntity> productList) {
 
         List<ProductResponse> productResponses = new ArrayList<>();
 
-        for(Product product : productList) {
+        for(ProductEntity product : productList) {
             Integer price = product.getSellings().stream()
                     .map(Selling::getPrice)
                     .min(Integer::compareTo)
@@ -152,7 +151,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
 
-    private List<SellingResponse> getSellingList(Product product) {
+    private List<SellingResponse> getSellingList(ProductEntity product) {
         List<SellingResponse> sellingResponses = new ArrayList<>();
 
         for (Selling selling : product.getSellings()) {
