0dc732e62458c6363393fbaea280d594a9f18fc8
jina <jina6962@gmail.com>
2021-11-03 10:34:07 +0900

    [ADD] - Kakao api response

---
+++ ssgp/src/main/java/project/ssgp/map/payload/response/MetaResponse.java
@@ -0,0 +1,14 @@
+package project.ssgp.map.payload.response;
+
+
+public class MetaResponse {
+
+    private Integer totalCount;
+
+    private Integer pageableCount;
+
+    private boolean isEnd;
+
+    private SameNameResponse sameName;
+
+}
