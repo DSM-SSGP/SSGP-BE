d321a0b9170da1f1174ee7054bdc5ff217099670
jina <jina6962@gmail.com>
2021-11-08 10:04:21 +0900

    [FIX] - type 변경

--- ssgp/src/main/java/project/ssgp/user/repository/UserRepository.java
+++ ssgp/src/main/java/project/ssgp/user/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import project.ssgp.user.entity.UserEntity;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
